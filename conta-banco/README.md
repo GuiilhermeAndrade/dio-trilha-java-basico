8ff1a00f04fc749788a15ef78971acf8babcade4
GuiilhermeAndrade <guilhermeandrade0104@gmail.com>
2023-09-13 17:25:01 -0300

    Implementação conta bancária através do terminal

--- conta-banco/README.md
+++ conta-banco/README.md
@@ -1,18 +1,5 @@
-## Getting Started
+## SEJAM BEM-VINDOS AO MEU PRIMEIRO PROJETO NO GITHUB!
+  
+- Esta é uma implementação de uma conta bancária sendo simulada através do terminal/console!
 
-Welcome to the VS Code Java world. Here is a guideline to help you get started to write Java code in Visual Studio Code.
-
-## Folder Structure
-
-The workspace contains two folders by default, where:
-
-- `src`: the folder to maintain sources
-- `lib`: the folder to maintain dependencies
-
-Meanwhile, the compiled output files will be generated in the `bin` folder by default.
-
-> If you want to customize the folder structure, open `.vscode/settings.json` and update the related settings there.
-
-## Dependency Management
-
-The `JAVA PROJECTS` view allows you to manage your dependencies. More details can be found [here](https://github.com/microsoft/vscode-java-dependency#manage-dependencies).
+- As informações sobre o usuário foram obtidas através da Classe Scanner, posteriormente elas foram exibidas ao usuário e foi mostrado a mensagem final de conta criada. 
--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,37 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+public static void main(String[] args) {
+     
+     //Criando objeto Scanner para obter os valores digitados no terminal
+     Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+    
+        System.out.println("Por favor, digite o número da sua conta!");
+        int numeroConta = scanner.nextInt();
+
+        System.out.println("Por favor, digite o número da sua Agência!");
+        String Agência = scanner.next();
+
+        System.out.println("Por favor, digite o nome do responsável pela conta!");
+        String nomeCliente = scanner.next();
+
+        System.out.println("Por favor, digite o sobrenome do responsável pela conta!");
+        String sobrenomeCliente = scanner.next();
+
+        System.out.println("Por favor, digite o saldo da sua conta!");
+        Double saldoConta = scanner.nextDouble();
+
+     scanner.close();
+     
+     //exibindo as informações para o usuário
+     System.out.println("O número da sua conta é" + " " + numeroConta);
+     System.out.println("O número da sua agência é" + " " + Agência);
+     System.out.println("O nome do responsável pela conta é" + " " + nomeCliente + " " + sobrenomeCliente);
+     System.out.println("O saldo da sua conta é" + " " + "R$" + saldoConta);
+     
+     //imprimindo a mensagem final de conta criada
+    System.out.println("Olá" + " " + nomeCliente + " " + sobrenomeCliente +", obrigado por criar uma conta em nosso banco, sua agência é" + " "+ Agência + ", conta" + " " + numeroConta + " " + "e seu saldo de" + " " + "R$" + saldoConta + " " +"já está disponível para saque.");
+     }
+     
     }
-}
